369718bbebf0ce05f1b15b525f355e6ea689cfd7
Rishav Dhara <mr.oopsthefirst17@gmail.com>
2025-07-07 02:46:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,6 @@ def upload():
         return f"Error: {e}"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if not set
+    app.run(host="0.0.0.0", port=port)
+
